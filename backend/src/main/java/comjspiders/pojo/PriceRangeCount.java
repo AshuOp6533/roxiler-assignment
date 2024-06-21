b965bbd3e8ebc0d9ad16250eda0fffd2d6986c90
AshuOp6533 <aashusin0015@gmail.com>
2024-06-21 18:39:57 +0530

    project

---
+++ backend/src/main/java/comjspiders/pojo/PriceRangeCount.java
@@ -0,0 +1,11 @@
+package comjspiders.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PriceRangeCount {
+    private String priceRange;
+    private long itemCount;
+}
